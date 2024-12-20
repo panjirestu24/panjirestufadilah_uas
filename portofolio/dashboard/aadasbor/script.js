a52a892af3acaef2a392e18a1bb58853dab156c1
panjirestu24 <panjirestufadilah@gmail.com>
2024-12-20 17:20:29 +0700

    Add files via upload

---
+++ portofolio/dashboard/aadasbor/script.js
@@ -0,0 +1,15 @@
+document.querySelector('.add-post-btn').addEventListener('click', function() {
+    alert('Add new post functionality not implemented in this demo.');
+});
+
+document.querySelectorAll('.edit-btn').forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        alert('Edit functionality not implemented in this demo.');
+    });
+});
+
+document.querySelectorAll('.nav-item').forEach(function(item) {
+    item.addEventListener('click', function() {
+        alert('Navigation functionality not implemented in this demo.');
+    });
+});
